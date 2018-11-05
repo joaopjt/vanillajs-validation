2ba583a4a2abb39dcc86fb24642ec95c5643da44
Joao Julio <joao.tavares@novahaus.com.br>
2018-11-05 10:38:14 -0200

    Added default messages for digits and CNPJ rules

--- resources/default-messages.js
+++ resources/default-messages.js
@@ -2,6 +2,8 @@
 export default {
   cep: 'CEP inválido.',
   cpf: 'CPF inválido.',
+  cnpj: 'CNPJ inválido.',
+  digits: 'Apenas dígitos permitidos.',
   email: 'Email inválido.',
   required: 'Campo obrigatório.',
   minlength(n) {
